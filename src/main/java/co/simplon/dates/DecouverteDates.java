0ff0fea37fdb2b9c6b30a2687fff082a86986cac
STEPHANE50 <stephane.levasseur@pole-emploi.fr>
2017-03-08 17:19:45 +0100

    commit 1

--- src/main/java/co/simplon/dates/DecouverteDates.java
+++ src/main/java/co/simplon/dates/DecouverteDates.java
@@ -1,7 +1,13 @@
 package co.simplon.dates;
 
+import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+
+import javax.print.attribute.standard.RequestingUserName;
 
 public class DecouverteDates {
 
@@ -9,32 +15,32 @@ public class DecouverteDates {
 		super();
 	}
 
-	public boolean estInferieurDateCourante(Date date) throws ParseException {
-		// TODO
-		throw new RuntimeException("à implémenter");
+	public boolean estInferieurDateCourante(Date date) {
+		Date aujourdhui = new Date();
+		return (date.compareTo(aujourdhui) == -1);
 	}
 
-	public Date construireDate(String chaineFournie) {
-		// TODO
-		throw new RuntimeException("à implémenter");
+	public Date construireDate(String chaineFournie) throws ParseException {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		return dateFormat.parse(chaineFournie);
+		
+		
 	}
-
-	public Date augmenterDate(Date dateInitiale, int nombreJours,
-			int nombreHeures, int nombreMinutes) {
+	public Date augmenterDate(Date dateInitiale, int nombreJours, int nombreHeures, int nombreMinutes) {
 		// TODO
 		throw new RuntimeException("à implémenter");
 	}
 
 	public String formaterUneDate(Date date) {
-		// TODO
-		return "";
+		return null;
+		
 	}
 
 	public String formaterUneHeure(Date time) {
 		// TODO
 		return "";
 	}
-	
+
 	public String formaterDateEnFrancais(Date date) {
 		// TODO
 		return "";
